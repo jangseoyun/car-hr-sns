40560c1a9dc35b3b0d68dd73b489d8df9840b488
seoyunjang <may-primavera@naver.com>
2024-02-10 04:26:22 +0900

    test: #11 Spring Data REST Test 제외
    - spring Data REST test 통합테스트는 공부 목적으로 작성한 테스트로 제외 처리를 하여 전체 테스트에서 제외한다.
    
    #11

--- src/test/java/com/car/sns/controller/DataRestTest.java
+++ src/test/java/com/car/sns/controller/DataRestTest.java
@@ -1,5 +1,6 @@
 package com.car.sns.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("Data rest API test")
+@Disabled("Spring Data REST 통합테스트는 불필요하므로 제외한다")
 @Transactional
 @AutoConfigureMockMvc
 @SpringBootTest
