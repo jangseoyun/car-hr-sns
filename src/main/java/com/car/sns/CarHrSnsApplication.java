18f7fb5bffe9417ea6d2d96f49f73749c57866ee
seoyunjang <may-primavera@naver.com>
2024-02-09 19:38:13 +0900

    feat: 프로젝트 환경 설정
     - Java 17, springboot 3.2.2
    
    #2

---
+++ src/main/java/com/car/sns/CarHrSnsApplication.java
@@ -0,0 +1,13 @@
+package com.car.sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarHrSnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarHrSnsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/car/sns/CarHrSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.car.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarHrSnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
