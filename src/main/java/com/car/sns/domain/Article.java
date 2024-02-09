a1732695f81cd8efe51d8ed55aa9606e2a71b755
seoyunjang <may-primavera@naver.com>
2024-02-10 01:38:56 +0900

    test: JPA DB CRUD 테스트 작성
    - 테스트 데이터 등록
    - 데이터 등록
    - 데이터 조회
    - 데이터 삭제
    - 데이터 수정
    
    #9

--- src/main/java/com/car/sns/domain/Article.java
+++ src/main/java/com/car/sns/domain/Article.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.HashSet;
@@ -14,6 +15,7 @@ import java.util.Set;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @ToString
 @Table(name = "t_article",
---
+++ src/test/java/com/car/sns/repository/JpaRepositoryTest.java
@@ -0,0 +1,89 @@
+package com.car.sns.repository;
+
+import com.car.sns.config.JpaConfig;
+import com.car.sns.domain.Article;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Import(JpaConfig.class)
+@DataJpaTest
+@DisplayName("JPA 연결 테스트.")
+class JpaRepositoryTest {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    public JpaRepositoryTest(
+            @Autowired  ArticleRepository articleRepository,
+            @Autowired ArticleCommentRepository articleCommentRepository)
+    {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @Test
+    @DisplayName("select test")
+    void givenTestData_whenSelecting_thenWorksFine() {
+
+        List<Article> articles = articleRepository.findAll();
+
+        assertThat(articles)
+                .isNotNull()
+                .hasSize(123);
+    }
+
+    @Test
+    @DisplayName("insert test")
+    void givenTestData_whenInserting_thenWorksFine() {
+
+        long previousCount = articleRepository.count();
+        Article article = Article.of("new title", "new content", "hashtag");
+
+        Article savedArticle = articleRepository.save(article);
+
+        assertThat(articleRepository.count())
+                .isNotNull()
+                .isEqualTo(previousCount + 1);
+    }
+
+    @Test
+    @DisplayName("update test")
+    void givenTestData_whenUpdating_thenWorksFine() {
+
+        Article article = articleRepository.findById(1L).orElseThrow();
+
+        String updateHashtag = "#springboot";
+        article.setHashtag(updateHashtag);
+
+        Article savedArticle = articleRepository.saveAndFlush(article);
+
+        assertThat(savedArticle)
+                .hasFieldOrPropertyWithValue("hashtag", updateHashtag);
+    }
+
+    @Test
+    @DisplayName("delete test")
+    void givenTestData_whenDeleting_thenWorksFine() {
+
+        Article article = articleRepository.findById(1L).orElseThrow();
+        long previousArticleCount = articleRepository.count();
+        long previousArticleCommentCount = articleCommentRepository.count();
+        int deletedCommentSize = article.getArticleComment().size();
+
+        articleRepository.delete(article);
+
+        assertThat(articleRepository.count())
+                .isEqualTo(previousArticleCount - 1);
+
+        assertThat(articleCommentRepository.count())
+                .isEqualTo(previousArticleCommentCount - deletedCommentSize);
+    }
+
+}
