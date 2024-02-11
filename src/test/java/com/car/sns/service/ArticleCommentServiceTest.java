1ed0a5d91784f60ce73c43f4bbdc146b35caa366
seoyunjang <may-primavera@naver.com>
2024-02-11 20:48:40 +0900

    test: 댓글 저장, 조회를 위한 테스트 구조 구현
    - 댓글 저장 및 조회 테스트를 위한 기본 동작에 대하여 service 테스트 하기 위해 구조를 잡는다
    
    #23

---
+++ src/test/java/com/car/sns/service/ArticleCommentServiceTest.java
@@ -0,0 +1,66 @@
+package com.car.sns.service;
+
+import com.car.sns.domain.Article;
+import com.car.sns.domain.ArticleComment;
+import com.car.sns.domain.ArticleCommentDto;
+import com.car.sns.repository.ArticleCommentRepository;
+import com.car.sns.repository.ArticleRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+import static org.mockito.BDDMockito.then;
+
+@DisplayName("비즈니스 로직 - 댓글")
+@ExtendWith(MockitoExtension.class)
+class ArticleCommentServiceTest {
+
+    @InjectMocks
+    private ArticleCommentService sut;
+    @Mock
+    private ArticleRepository articleRepository;
+    @Mock
+    private ArticleCommentRepository articleCommentRepository;
+
+    @DisplayName("게시글 ID로 조회시 관련 댓글을 반환한다")
+    @Test
+    void givenArticleId_whenSearchingArticleComments_thenArticleComments() {
+        //given
+        Long articleId = 1L;
+        Article article = Article.of("title", "content", "hashtag");
+
+        given(articleRepository.findById(articleId))
+                .willReturn(Optional.of(article));
+
+        //when
+        List<ArticleCommentDto> articleComments = sut.searchArticleComment(articleId);
+
+        //then
+        assertThat(articleComments).isNotNull();
+        then(articleRepository).should().findById(articleId);
+    }
+
+    @DisplayName("댓글 정보를 입력하면 댓글을 저장")
+    @Test
+    void givenArticleCommentInfo_whenSavingArticleComments_thenSaveArticleComment() {
+        //given
+        given(articleCommentRepository.save(any(ArticleComment.class)))
+                .willReturn(null);
+
+        //when
+        sut.saveArticleComment("content comment");
+
+        //then
+        then(articleCommentRepository).should().save(any(ArticleComment.class));
+    }
+}
