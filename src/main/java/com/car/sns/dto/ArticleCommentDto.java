12fead2ee5b46ed65d3eb12d6e0f94f0ac0c29bf
seoyunjang <may-primavera@naver.com>
2024-02-12 20:03:45 +0900

    feat: 댓글, 수정, 게시글_댓글 model 객체 생성
    - entity와 의존성을 낮추기 위해서 model 객체에서 데이터를 변환 가능하도록 구현
    - 단순 mapper 기능을 하는 메서드가 증가함에 따라 mapper 패키지를 생성할지 고려
    
    #28

--- src/main/java/com/car/sns/dto/ArticleCommentDto.java
+++ src/main/java/com/car/sns/dto/ArticleCommentDto.java
@@ -8,13 +8,39 @@ import java.time.LocalDateTime;
  * DTO for {@link ArticleComment}
  */
 public record ArticleCommentDto(
+        Long ArticleId,
+        UserAccountDto userAccountDto,
         LocalDateTime createdAt,
         String createdBy,
         LocalDateTime modifiedAt,
         String modifiedBy,
         String content
 ) {
-    public static ArticleCommentDto of(LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy, String content) {
-        return new ArticleCommentDto(createdAt, createdBy, modifiedAt, modifiedBy, content);
+    public ArticleCommentDto(Long ArticleId,
+                             UserAccountDto userAccountDto,
+                             LocalDateTime createdAt,
+                             String createdBy,
+                             LocalDateTime modifiedAt,
+                             String modifiedBy,
+                             String content) {
+        this.ArticleId = ArticleId;
+        this.userAccountDto = userAccountDto;
+        this.createdAt = createdAt;
+        this.createdBy = createdBy;
+        this.modifiedAt = modifiedAt;
+        this.modifiedBy = modifiedBy;
+        this.content = content;
+    }
+
+    public static ArticleCommentDto from(ArticleComment entity) {
+        return new ArticleCommentDto(
+                entity.getArticle().getId(),
+                UserAccountDto.from(entity.getUserAccount()),
+                entity.getCreatedAt(),
+                entity.getCreatedBy(),
+                entity.getModifiedAt(),
+                entity.getModifiedBy(),
+                entity.getContent()
+        );
     }
 }
