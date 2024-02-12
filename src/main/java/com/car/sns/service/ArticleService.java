06adfd938750e52c9b4497eb668f38a62bf4ff7b
seoyunjang <may-primavera@naver.com>
2024-02-12 20:01:15 +0900

    feat: 게시글 검색, 단건 조회 기능
    - 컨트롤러에서 게시글 전체 조회, 검색을 따로 구현하는것이 아니라 검색이 아닌 경우 전체 조회가 가능하도록 구현
    - 게시글 아이디로 조회 가능한 게시글 단건조회 구현
    - 단건 조회의 경우 게시글 내용과 조회 요청한 게시글에 관련된 댓글도 함께 나오도록 리펙토링 필요
    -  view에 호출이 가능한지 테스트 코드 작성
    
    #28

--- src/main/java/com/car/sns/service/ArticleService.java
+++ src/main/java/com/car/sns/service/ArticleService.java
@@ -1,9 +1,12 @@
 package com.car.sns.service;
 
+import com.car.sns.domain.Article;
 import com.car.sns.domain.type.SearchType;
 import com.car.sns.dto.ArticleDto;
 import com.car.sns.dto.ArticleModifyDto;
+import com.car.sns.dto.ArticleWithCommentDto;
 import com.car.sns.repository.ArticleRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -27,13 +30,44 @@ public class ArticleService {
             case TITLE -> articleRepository.findByTitleContaining(searchKeyword, pageable).map(ArticleDto::from);
             case CONTENT -> articleRepository.findByContentContaining(searchKeyword, pageable).map(ArticleDto::from);
             case ID -> articleRepository.findByHashtag(searchKeyword, pageable).map(ArticleDto::from);
-            case HASHTAG -> articleRepository.findByUserAccount_UserIdContaining(searchKeyword, pageable).map(ArticleDto::from);
-            case NICKNAME -> articleRepository.findByUserAccount_NicknameContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case HASHTAG ->
+                    articleRepository.findByUserAccount_UserIdContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case NICKNAME ->
+                    articleRepository.findByUserAccount_NicknameContaining(searchKeyword, pageable).map(ArticleDto::from);
         };
     }
 
-    public void updateArticle(long articleId, ArticleModifyDto articleModifyDto) {
+    @Transactional(readOnly = true)
+    public ArticleDto getArticle(Long articleId) {
+        return articleRepository.findById(articleId)
+                .map(ArticleDto::from)
+                .orElseThrow(() -> new EntityNotFoundException("게시글이 존재하지 않습니다: - articleId: " + articleId));
+    }
 
+    @Transactional(readOnly = true)
+    public ArticleWithCommentDto getArticleWithComments(Long articleId) {
+        return articleRepository.findById(articleId)
+                .map(ArticleWithCommentDto::from)
+                .orElseThrow(() -> new EntityNotFoundException("게시글이 존재하지 않습니다: - articleId: " + articleId));
+    }
+
+    public void createArticle(ArticleDto articleDto) {
+        articleRepository.save(articleDto.toEntity());
+    }
+
+    @Transactional(readOnly = true)
+    public void updateArticle(ArticleModifyDto articleModifyDto) {
+        try {
+            Article article = articleRepository.getReferenceById(articleModifyDto.articleId());
+
+            if (articleModifyDto.userAccountId() == article.getUserAccount().getUserAccountId()) {
+                if (articleModifyDto.title() != null) {article.setTitle(articleModifyDto.title());}
+                if (articleModifyDto.content() != null) {article.setTitle(articleModifyDto.content());}
+                if (articleModifyDto.hashtag() != null) {article.setTitle(articleModifyDto.hashtag());}
+            }
+        } catch (EntityNotFoundException e) {
+                System.out.println(e);
+        }
     }
 
     public void deleteArticle(long articleId) {
--- src/test/java/com/car/sns/service/ArticleServiceTest.java
+++ src/test/java/com/car/sns/service/ArticleServiceTest.java
@@ -1,8 +1,12 @@
 package com.car.sns.service;
 
+import com.car.sns.domain.Article;
+import com.car.sns.domain.UserAccount;
 import com.car.sns.domain.type.SearchType;
 import com.car.sns.dto.ArticleDto;
+import com.car.sns.dto.ArticleModifyDto;
 import com.car.sns.repository.ArticleRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,9 +16,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
+import static org.mockito.BDDMockito.*;
 
 @DisplayName("비즈니스 로직 - 게시글")
 @ExtendWith(MockitoExtension.class)
@@ -23,6 +28,11 @@ class ArticleServiceTest {
     private ArticleService sut;
     @Mock
     private ArticleRepository articleRepository;
+
+    @BeforeEach
+    void setUp() {
+
+    }
 
     @DisplayName("[검색] - 검색어 없이 게시글을 조회할 경우 게시글 페이지 반환")
     @Test
@@ -54,32 +64,59 @@ class ArticleServiceTest {
         then(articleRepository).should().findByTitleContaining("title", pageable);
     }
 
-    /*@DisplayName("게시글 정보를 입력하면 게시글을 생성한다")
+    @DisplayName("[조회] - 게시글 ID 입력시 해당 게시글 단건 반환")
     @Test
-    void givenArticleInfo_whenSavingArticle_thenReturnArticle() {
+    void givenArticleId_whenSelectingArticle_thenReturnArticle() {
         //given
-        ArticleDto articleDto = ArticleDto.of(
-                LocalDateTime.now(),
-                "seo-test",
-                "test-title",
-                "test-content",
-                "hashtag"
-        );
+        Long articleId = 1L;
+        Article article = createdArticle();
+        given(articleRepository.findById(articleId))
+                .willReturn(Optional.of(article));
 
+        //when
+        sut.getArticle(articleId);
+
+        //then
+        then(articleRepository)
+                .should()
+                .findById(articleId);
+    }
+
+    @DisplayName("[조회] - 게시글 ID 입력시 해당 게시글과 댓글 리스트 반환")
+    @Test
+    void givenArticleId_whenSelectingArticleAndComments_thenReturnArticleAndComments() {
+        //given
+        Long articleId = 1L;
+        Article article = createdArticle();
+        given(articleRepository.findById(articleId))
+                .willReturn(Optional.of(article));
+
+        //when
+        sut.getArticleWithComments(articleId);
+
+        //then
+        then(articleRepository)
+                .should()
+                .findById(articleId);
+    }
+
+    @DisplayName("게시글의 정보를 입력하면, 게시글을 저장한다")
+    @Test
+    void givenArticleInfo_whenSavingArticle_thenSavedArticle() {
+        //given
+        Article article = createdArticle();
         given(articleRepository.save(any(Article.class)))
                 .willReturn(null);
 
         //when
-        sut.searchArticles(articleDto);
+        sut.createArticle(createdArticleDto(article));
 
         //then
-        // save method가 호출되었는지 확인하는 것
         then(articleRepository)
                 .should()
                 .save(any(Article.class));
     }
 
-    @DisplayName("게시글의 ID와 수정 정보를 입력하면, 게시글을 수정한다")
     @Test
     void givenArticleIdAndModifiedInfo_whenUpdatingArticle_thenUpdateArticle() {
         //given
@@ -87,14 +124,29 @@ class ArticleServiceTest {
                 .willReturn(null);
 
         //when
-        sut.updateArticle(1L, ArticleModifyDto.of("title", "content", "hashtag"));
+        sut.updateArticle(ArticleModifyDto.of(1L, 1L, "title", null, null));
 
         //then
-        //save method가 호출되었는지 확인하는 것
         then(articleRepository)
                 .should()
                 .save(any(Article.class));
     }
+
+    private Article createdArticle() {
+        UserAccount userAccount = createdUserAccount();
+        return Article.of(userAccount, "title", "content", "hashtag");
+    }
+
+    private UserAccount createdUserAccount() {
+        return UserAccount.of("userId", "userPassword", "user@email.com", "nickname", null);
+    }
+
+    private ArticleDto createdArticleDto(Article article) {
+        return ArticleDto.from(article);
+    }
+
+    /* @DisplayName("게시글의 ID와 수정 정보를 입력하면, 게시글을 수정한다")
+
 
     @DisplayName("게시글의 ID를 입력하면 게시글을 삭제한다")
     @Test
