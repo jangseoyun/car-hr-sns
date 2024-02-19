b2c63d7968a4f387f241679745e1fe0a5e928d81
seoyunjang <may-primavera@naver.com>
2024-02-20 01:50:20 +0900

    refact: userAccount domain index 추가
    - 검색을 위해 UserId 인덱스 컬럼 추가
    
    #37

--- src/main/java/com/car/sns/domain/UserAccount.java
+++ src/main/java/com/car/sns/domain/UserAccount.java
@@ -14,6 +14,7 @@ import java.util.Objects;
 @ToString(callSuper = true)
 @Table(name = "t_user_account",
         indexes = {
+                @Index(columnList = "userId", unique = true),
                 @Index(columnList = "email", unique = true),
                 @Index(columnList = "createdAt"),
                 @Index(columnList = "createdBy")
