bb65cf706635bbcf2b9f529b42658e4618de494a
seoyunjang <may-primavera@naver.com>
2024-02-23 03:03:45 +0900

    feat: 대댓글 도메인 설계
    - 단방향 매핑으로 새로운 테이블을 생성하지 않고 oneToMany로 entity 설계
    - ERD 수정
    
    #67

--- src/main/java/com/car/sns/domain/comment/entity/ArticleComment.java
+++ src/main/java/com/car/sns/domain/comment/entity/ArticleComment.java
@@ -1,15 +1,17 @@
 package com.car.sns.domain.comment.entity;
 
 import com.car.sns.common.AuditingFields;
-import com.car.sns.domain.user.entity.UserAccount;
 import com.car.sns.domain.board.entity.Article;
+import com.car.sns.domain.user.entity.UserAccount;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 
 @Entity
@@ -28,6 +30,15 @@ public class ArticleComment extends AuditingFields {
     @Column(name = "article_comment_id")
     private Long id;
 
+    @Column(name = "parent_comment_id", updatable = false)
+    private Long parentCommentId;
+
+    //TODO: cascade 보수적으로 도메인에 맞게 변경할 것
+    @ToString.Exclude
+    @OrderBy("createdAt ASC")
+    @OneToMany(mappedBy = "parentCommentId", cascade = CascadeType.ALL)
+    private Set<ArticleComment> childComments = new LinkedHashSet<>();
+
     @ManyToOne(optional = false)
     @JoinColumn(name = "article_id")
     private Article article;
@@ -41,14 +52,24 @@ public class ArticleComment extends AuditingFields {
     @JoinColumn(name = "user_account_id")
     private UserAccount userAccount;
 
-    private ArticleComment(UserAccount userAccount, Article article, String content) {
+    private ArticleComment(UserAccount userAccount, Long parentCommentId, Article article, String content) {
         this.userAccount = userAccount;
         this.article = article;
+        this.parentCommentId = parentCommentId;
         this.content = content;
     }
 
     public static ArticleComment of(UserAccount userAccount, Article article, String content) {
-        return new ArticleComment(userAccount, article, content);
+        return new ArticleComment(userAccount, null, article, content);
+    }
+
+    public void addChildComment(ArticleComment childComment) {
+        childComment.updateParentCommentId(this.id);
+        this.getChildComments().add(childComment);
+    }
+
+    private void updateParentCommentId(Long parentCommentId) {
+        this.parentCommentId = parentCommentId;
     }
 
     @Override
