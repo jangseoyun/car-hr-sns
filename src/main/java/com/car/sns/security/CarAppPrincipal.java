1cea2f01af0a6d5b9bcd9be5057aa2a96be0c4b8
seoyunjang <may-primavera@naver.com>
2024-02-23 23:12:57 +0900

    feat: 카카오 인증 응답 정보 확인 / 정의
    - 카카오 로그인 인증 응답 DTO 생성 후 반환되는 응답 정보 확인
    
    #70

--- src/main/java/com/car/sns/security/CarAppPrincipal.java
+++ src/main/java/com/car/sns/security/CarAppPrincipal.java
@@ -4,8 +4,10 @@ import com.car.sns.domain.user.model.UserAccountDto;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -16,11 +18,12 @@ public record CarAppPrincipal(
         String password,
         String email,
         String nickname,
-        String memo
+        String memo,
+        Map<String, Object> oAuth2Attribute
 
-) implements UserDetails {
+) implements UserDetails, OAuth2User {
 
-    public static CarAppPrincipal of(String username, String password, String email, String nickname, String memo) {
+    public static CarAppPrincipal of(String username, String password, String email, String nickname, String memo, Map<String, Object> oAuth2Attribute) {
         Set<RoleType> roleTypes = Set.of(RoleType.USER);
 
         return new CarAppPrincipal(
@@ -32,7 +35,12 @@ public record CarAppPrincipal(
                 password,
                 email,
                 nickname,
-                memo);
+                memo,
+                oAuth2Attribute);
+    }
+
+    public static CarAppPrincipal of(String username, String password, String email, String nickname, String memo) {
+        return CarAppPrincipal.of(username, password, email, nickname, memo, Map.of());
     }
 
     public static CarAppPrincipal from(UserAccountDto userAccountDto) {
@@ -48,12 +56,23 @@ public record CarAppPrincipal(
         return UserAccountDto.of(username, password, email, nickname, memo);
     }
 
+    /**
+     * OAuth2 method
+     */
+
     @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        //권한
-        return null;
+    public Map<String, Object> getAttributes() {
+        return oAuth2Attribute;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    /**
+     * Spring security method
+     */
     @Override
     public String getPassword() {
         return password;
@@ -82,5 +101,10 @@ public record CarAppPrincipal(
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    @Override
+    public String getName() {
+        return username;
     }
 }
