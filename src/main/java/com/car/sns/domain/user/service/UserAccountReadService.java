30706bc6af3adbf65f5c53a9967900395d65e6b1
seoyunjang <may-primavera@naver.com>
2024-02-29 05:39:06 +0900

    refact: filter단에서 사용자 검색시 캐시 확인
    - 기존 DB에서 데이터를 조회하던 searchUser 메서드에서 1차적으로 cache에서 해당 사용자의 정보를 조회한 뒤 존재하지 않으면 DB에 가져와서 캐시에 저장하도록 구현
    
    #106

--- src/main/java/com/car/sns/domain/user/service/UserAccountReadService.java
+++ src/main/java/com/car/sns/domain/user/service/UserAccountReadService.java
@@ -3,10 +3,11 @@ package com.car.sns.domain.user.service;
 import com.car.sns.application.usecase.user.UserReadUseCase;
 import com.car.sns.domain.user.model.LoginDto;
 import com.car.sns.domain.user.model.UserAccountDto;
-import com.car.sns.domain.user.model.entity.UserAccount;
 import com.car.sns.exception.CarHrSnsAppException;
+import com.car.sns.infrastructure.UserCacheRepository;
 import com.car.sns.infrastructure.jpaRepository.UserAccountJpaRepository;
 import com.car.sns.presentation.model.response.UserLoginResponse;
+import com.car.sns.security.CarAppPrincipal;
 import com.car.sns.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -31,33 +32,37 @@ public class UserAccountReadService implements UserReadUseCase {
     private long expireTimeMs = 1000 * 60 * 60; //TODO:test를 위한것으로 Redis implement
     private final PasswordEncoder passwordEncoder;
     private final UserAccountJpaRepository userAccountJpaRepository;
+    private final UserCacheRepository userCacheRepository;
 
     /**
      *  Optional로 반환하는 이유는 해당 service에서 결정하는 것이 아니라
      *  user만 찾고 이후 단계는 앞단에서 맞는 exception으로 맞춰줄 것
+     *  캐시에 올라간 상태인지 확인 후 DB 요청
      */
-    public Optional<UserAccountDto> searchUser(String username) {
-        return userAccountJpaRepository.findByUserId(username)
-                .map(UserAccountDto::from);
+    public Optional<CarAppPrincipal> searchUser(String username) {
+        CarAppPrincipal carAppPrincipal = userCacheRepository.getUser(username).orElseGet(() ->
+                userAccountJpaRepository.findByUserId(username)
+                        .map(UserAccountDto::from)
+                        .map(CarAppPrincipal::from)
+                        .orElseThrow(() -> {
+                            throw new CarHrSnsAppException(USER_NOTFOUND_ACCOUNT, USER_NOTFOUND_ACCOUNT.getMessage());
+                        })
+        );
+        return Optional.of(carAppPrincipal);
     }
 
     @Override
     public UserLoginResponse login(LoginDto loginDto) {
-        UserAccountDto userAccountDto = userAccountJpaRepository.findByUserId(loginDto.userId())
+        userAccountJpaRepository.findByUserId(loginDto.userId())
                 .filter(user -> passwordEncoder.matches(loginDto.password(), user.getUserPassword()))
                 .map(UserAccountDto::from)
-                .orElseThrow(() -> {
+                .ifPresentOrElse(
+                        userAccountDto -> userCacheRepository.setUser(CarAppPrincipal.from(userAccountDto)),
+                        () -> {
                     throw new CarHrSnsAppException(INVALID_USER_ID_PASSWORD, INVALID_USER_ID_PASSWORD.getMessage());
                 });
 
-        log.info("login userAccountDto: {}", userAccountDto);
-        return  UserLoginResponse.of(JwtUtil.createToken(userAccountDto.userId(), secretKey, expireTimeMs));
-    }
-
-    public UserAccount loadUserByUserName(String userId) {
-        return userAccountJpaRepository.findByUserId(userId)
-                .orElseThrow(
-                        () -> new CarHrSnsAppException(USER_NOTFOUND_ACCOUNT, USER_NOTFOUND_ACCOUNT.getMessage() + ":" + userId)
-                );
+        log.info("login userAccountDto: {}", loginDto);
+        return  UserLoginResponse.of(JwtUtil.createToken(loginDto.userId(), secretKey, expireTimeMs));
     }
 }
