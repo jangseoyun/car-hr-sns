ab9ee5beb5c31395ae575126b8d176d97b88aa30
seoyunjang <may-primavera@naver.com>
2024-03-01 22:17:07 +0900

    feat: logback 적용할 log module 생성
    - log 모듈 생성
    
    #113

---
+++ car-hr-log/src/main/java/com/car/log/CarHrLogApplication.java
@@ -0,0 +1,13 @@
+package com.car.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarHrLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarHrLogApplication.class, args);
+    }
+
+}
